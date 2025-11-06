62d35b7aa4747cea97a5c769adf1e27b2ecae165
Тимур <worlsait@gmail.com>
2025-11-06 02:13:18 -0100

    1

--- 322_Khamukov/MainWindow.xaml.cs
+++ 322_Khamukov/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,57 @@ namespace _322_Khamukov
         public MainWindow()
         {
             InitializeComponent();
+            ApplyTheme("Dictionary.xaml");
+            ThemeComboBox.SelectedIndex = 0;
+            MainFrame.Navigate(new Pages.AuthPage());
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => {
+                DateTimeNow.Text = DateTime.Now.ToString();
+            };
+            timer.Start();
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+
+        void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите закрыть окно?", "Message",
+            MessageBoxButton.YesNo) == System.Windows.MessageBoxResult.No)
+                e.Cancel = true;
+            else
+
+                e.Cancel = false;
+        }
+
+        private void ApplyTheme(string themeFile)
+        {
+            var uri = new Uri(themeFile, UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void ThemeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ThemeComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string themeFile = selectedItem.Tag.ToString();
+                ApplyTheme(themeFile);
+            }
+        }
+
+
     }
 }
